dc64683498e11fd82df063bcb8c9ab30b45f1163
Robert Soncrant <devreaper0@gmail.com>
2024-10-18 02:43:10 -0500

    Fixed some more issues by refactoring.

--- db/postgresql.go
+++ db/postgresql.go
@@ -209,16 +209,16 @@ func (db *PostgreSQLDatabase) Connect() (err error) {
 	return
 }
 
-func (db PostgreSQLDatabase) Close() error {
+func (db *PostgreSQLDatabase) Close() error {
 	logging.Info().Msg("Closing PostgreSQL connection...")
 	return db.sqlDB.Close()
 }
 
-func (db PostgreSQLDatabase) EngineName() string {
+func (db *PostgreSQLDatabase) EngineName() string {
 	return "PostgreSQL"
 }
 
-func (db PostgreSQLDatabase) GetTrack(id string) (types.Track, error) {
+func (db *PostgreSQLDatabase) GetTrack(id string) (types.Track, error) {
 	track := types.Track{}
 	row := db.sqlDB.QueryRow("SELECT * FROM tracks WHERE id=$1;", id)
 	err := row.Scan(&track.ID, &track.UserID, &track.ISRC, &track.Title, &track.ArtistIDs, &track.AlbumIDs, &track.PrimaryAlbumID, &track.TrackNumber, &track.Duration, &track.Description, &track.ReleaseDate, &track.Lyrics, &track.ListenCount, &track.FavoriteCount, &track.AdditionDate, &track.Tags, &track.AdditionalMeta, &track.Permissions, &track.LinkedItemIDs, &track.ContentSource, &track.MetadataSource, &track.LyricSources)
@@ -226,7 +226,7 @@ func (db PostgreSQLDatabase) GetTrack(id string) (types.Track, error) {
 	return track, err
 }
 
-func (db PostgreSQLDatabase) GetTracks(userID string) ([]types.Track, error) {
+func (db *PostgreSQLDatabase) GetTracks(userID string) ([]types.Track, error) {
 	var tracks []types.Track
 	rows, err := db.sqlDB.Query("SELECT * FROM tracks WHERE user_id=$1;", userID)
 	if err != nil {
@@ -244,7 +244,7 @@ func (db PostgreSQLDatabase) GetTracks(userID string) ([]types.Track, error) {
 	return tracks, err
 }
 
-func (db PostgreSQLDatabase) GetAllTracks() ([]types.Track, error) {
+func (db *PostgreSQLDatabase) GetAllTracks() ([]types.Track, error) {
 	var tracks []types.Track
 	rows, err := db.sqlDB.Query("SELECT * FROM tracks;")
 	if err != nil {
@@ -262,7 +262,7 @@ func (db PostgreSQLDatabase) GetAllTracks() ([]types.Track, error) {
 	return tracks, err
 }
 
-func (db PostgreSQLDatabase) AddTrack(track types.Track) error {
+func (db *PostgreSQLDatabase) AddTrack(track types.Track) error {
 	_, err := db.sqlDB.Exec(`
 	INSERT INTO tracks (
 	  id, user_id, isrc, title, artist_ids, album_ids, primary_album_id, track_number, duration, description, release_date, lyrics, listen_count, favorite_count, addition_date, tags, additional_meta, permissions, linked_item_ids, content_source, metadata_source, lyric_sources
@@ -273,7 +273,7 @@ func (db PostgreSQLDatabase) AddTrack(track types.Track) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UpdateTrack(track types.Track) error {
+func (db *PostgreSQLDatabase) UpdateTrack(track types.Track) error {
 	_, err := db.sqlDB.Exec(`
 	UPDATE tracks
 	SET user_id=$2, isrc=$3, title=$4, artist_ids=$5, album_ids=$6, primary_album_id=$7, track_number=$8, duration=$9, description=$10, release_date=$11, lyrics=$12, listen_count=$13, favorite_count=$14, addition_date=$15, tags=$16, additional_meta=$17, permissions=$18, linked_item_ids=$19, content_source=$20, metadata_source=$21, lyric_sources=$22
@@ -282,14 +282,14 @@ func (db PostgreSQLDatabase) UpdateTrack(track types.Track) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) GetAlbum(id string) (types.Album, error) {
+func (db *PostgreSQLDatabase) GetAlbum(id string) (types.Album, error) {
 	album := types.Album{}
 	row := db.sqlDB.QueryRow("SELECT * FROM albums WHERE id=$1;", id)
 	err := row.Scan(&album.ID, &album.UserID, &album.UPC, &album.Title, &album.ArtistIDs, &album.TrackIDs, &album.Description, &album.ReleaseDate, &album.ListenCount, &album.FavoriteCount, &album.AdditionDate, &album.Tags, &album.AdditionalMeta, &album.Permissions, &album.LinkedItemIDs, &album.MetadataSource)
 	return album, err
 }
 
-func (db PostgreSQLDatabase) GetAlbums(userID string) ([]types.Album, error) {
+func (db *PostgreSQLDatabase) GetAlbums(userID string) ([]types.Album, error) {
 	var albums []types.Album
 	rows, err := db.sqlDB.Query("SELECT * FROM albums WHERE user_id=$1;", userID)
 	if err != nil {
@@ -307,7 +307,7 @@ func (db PostgreSQLDatabase) GetAlbums(userID string) ([]types.Album, error) {
 	return albums, err
 }
 
-func (db PostgreSQLDatabase) GetAllAlbums() ([]types.Album, error) {
+func (db *PostgreSQLDatabase) GetAllAlbums() ([]types.Album, error) {
 	var albums []types.Album
 	rows, err := db.sqlDB.Query("SELECT * FROM albums;")
 	if err != nil {
@@ -325,7 +325,7 @@ func (db PostgreSQLDatabase) GetAllAlbums() ([]types.Album, error) {
 	return albums, err
 }
 
-func (db PostgreSQLDatabase) AddAlbum(album types.Album) error {
+func (db *PostgreSQLDatabase) AddAlbum(album types.Album) error {
 	_, err := db.sqlDB.Exec(`
 	INSERT INTO albums (
 	  id, user_id, upc, title, artist_ids, track_ids, description, release_date, listen_count, favorite_count, addition_date, tags, additional_meta, permissions, linked_item_ids, metadata_source
@@ -336,7 +336,7 @@ func (db PostgreSQLDatabase) AddAlbum(album types.Album) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UpdateAlbum(album types.Album) error {
+func (db *PostgreSQLDatabase) UpdateAlbum(album types.Album) error {
 	_, err := db.sqlDB.Exec(`
 	UPDATE albums
 	SET user_id=$2, upc=$3, title=$4, artist_ids=$5, track_ids=$6, description=$7, release_date=$8, listen_count=$9, favorite_count=$10, addition_date=$11, tags=$12, additional_meta=$13, permissions=$14, linked_item_ids=$15, metadata_source=$16
@@ -345,7 +345,7 @@ func (db PostgreSQLDatabase) UpdateAlbum(album types.Album) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) GetVideo(id string) (types.Video, error) {
+func (db *PostgreSQLDatabase) GetVideo(id string) (types.Video, error) {
 	video := types.Video{}
 	row := db.sqlDB.QueryRow("SELECT * FROM videos WHERE id=$1;", id)
 	err := row.Scan(&video.ID, &video.UserID, &video.Title, &video.ArtistIDs, &video.Duration, &video.Description, &video.ReleaseDate, &video.Subtitles, &video.WatchCount, &video.FavoriteCount, &video.AdditionDate, &video.Tags, &video.AdditionalMeta, &video.Permissions, &video.LinkedItemIDs, &video.ContentSource, &video.MetadataSource, &video.LyricSources)
@@ -353,7 +353,7 @@ func (db PostgreSQLDatabase) GetVideo(id string) (types.Video, error) {
 	return video, err
 }
 
-func (db PostgreSQLDatabase) GetVideos(userID string) ([]types.Video, error) {
+func (db *PostgreSQLDatabase) GetVideos(userID string) ([]types.Video, error) {
 	var videos []types.Video
 	rows, err := db.sqlDB.Query("SELECT * FROM videos WHERE user_id=$1;", userID)
 	if err != nil {
@@ -371,7 +371,7 @@ func (db PostgreSQLDatabase) GetVideos(userID string) ([]types.Video, error) {
 	return videos, err
 }
 
-func (db PostgreSQLDatabase) GetAllVideos() ([]types.Video, error) {
+func (db *PostgreSQLDatabase) GetAllVideos() ([]types.Video, error) {
 	var videos []types.Video
 	rows, err := db.sqlDB.Query("SELECT * FROM videos;")
 	if err != nil {
@@ -389,7 +389,7 @@ func (db PostgreSQLDatabase) GetAllVideos() ([]types.Video, error) {
 	return videos, err
 }
 
-func (db PostgreSQLDatabase) AddVideo(video types.Video) error {
+func (db *PostgreSQLDatabase) AddVideo(video types.Video) error {
 	_, err := db.sqlDB.Exec(`
 	INSERT INTO videos (
 	  id, user_id, title, artist_ids, duration, description, release_date, subtitles, watch_count, favorite_count, addition_date, tags, additional_meta, permissions, linked_item_ids, content_source, metadata_source, lyric_sources
@@ -400,7 +400,7 @@ func (db PostgreSQLDatabase) AddVideo(video types.Video) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UpdateVideo(video types.Video) error {
+func (db *PostgreSQLDatabase) UpdateVideo(video types.Video) error {
 	_, err := db.sqlDB.Exec(`
 	UPDATE videos
 	SET user_id=$2, title=$3, artist_ids=$4, duration=$5, description=$6, release_date=$7, subtitles=$8, watch_count=$9, favorite_count=$10, addition_date=$11, tags=$12, additional_meta=$13, permissions=$14, linked_item_ids=$15, content_source=$16, metadata_source=$17, lyric_sources=$18
@@ -409,14 +409,14 @@ func (db PostgreSQLDatabase) UpdateVideo(video types.Video) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) GetArtist(id string) (types.Artist, error) {
+func (db *PostgreSQLDatabase) GetArtist(id string) (types.Artist, error) {
 	artist := types.Artist{}
 	row := db.sqlDB.QueryRow("SELECT * FROM artists WHERE id=$1;", id)
 	err := row.Scan(&artist.ID, &artist.UserID, &artist.Name, &artist.AlbumIDs, &artist.TrackIDs, &artist.Description, &artist.CreationDate, &artist.ListenCount, &artist.FavoriteCount, &artist.AdditionDate, &artist.Tags, &artist.AdditionalMeta, &artist.Permissions, &artist.LinkedItemIDs, &artist.MetadataSource)
 	return artist, err
 }
 
-func (db PostgreSQLDatabase) GetArtists(userID string) ([]types.Artist, error) {
+func (db *PostgreSQLDatabase) GetArtists(userID string) ([]types.Artist, error) {
 	var artists []types.Artist
 	rows, err := db.sqlDB.Query("SELECT * FROM artists WHERE user_id=$1;", userID)
 	if err != nil {
@@ -434,7 +434,7 @@ func (db PostgreSQLDatabase) GetArtists(userID string) ([]types.Artist, error) {
 	return artists, err
 }
 
-func (db PostgreSQLDatabase) GetAllArtists() ([]types.Artist, error) {
+func (db *PostgreSQLDatabase) GetAllArtists() ([]types.Artist, error) {
 	var artists []types.Artist
 	rows, err := db.sqlDB.Query("SELECT * FROM artists;")
 	if err != nil {
@@ -452,7 +452,7 @@ func (db PostgreSQLDatabase) GetAllArtists() ([]types.Artist, error) {
 	return artists, err
 }
 
-func (db PostgreSQLDatabase) AddArtist(artist types.Artist) error {
+func (db *PostgreSQLDatabase) AddArtist(artist types.Artist) error {
 	_, err := db.sqlDB.Exec(`
 	INSERT INTO artists (
 	  id, user_id, name, album_ids, track_ids, description, creation_date, listen_count, favorite_count, addition_date, tags, additional_meta, permissions, linked_item_ids, metadata_source
@@ -463,7 +463,7 @@ func (db PostgreSQLDatabase) AddArtist(artist types.Artist) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UpdateArtist(artist types.Artist) error {
+func (db *PostgreSQLDatabase) UpdateArtist(artist types.Artist) error {
 	_, err := db.sqlDB.Exec(`
 	UPDATE artists
 	SET user_id=$2, name=$3, album_ids=$4, track_ids=$5, description=$6, creation_date=$7, listen_count=$8, favorite_count=$9, addition_date=$10, tags=$11, additional_meta=$12, permissions=$13, linked_item_ids=$14, metadata_source=$15
@@ -472,14 +472,14 @@ func (db PostgreSQLDatabase) UpdateArtist(artist types.Artist) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) GetPlaylist(id string) (types.Playlist, error) {
+func (db *PostgreSQLDatabase) GetPlaylist(id string) (types.Playlist, error) {
 	playlist := types.Playlist{}
 	row := db.sqlDB.QueryRow("SELECT * FROM playlists WHERE id=$1;", id)
 	err := row.Scan(&playlist.ID, &playlist.UserID, &playlist.Title, &playlist.TrackIDs, &playlist.ListenCount, &playlist.FavoriteCount, &playlist.Description, &playlist.CreationDate, &playlist.AdditionDate, &playlist.Tags, &playlist.AdditionalMeta, &playlist.Permissions, &playlist.MetadataSource)
 	return playlist, err
 }
 
-func (db PostgreSQLDatabase) GetPlaylists(userID string) ([]types.Playlist, error) {
+func (db *PostgreSQLDatabase) GetPlaylists(userID string) ([]types.Playlist, error) {
 	var playlists []types.Playlist
 	rows, err := db.sqlDB.Query("SELECT * FROM playlists WHERE user_id=$1;", userID)
 	if err != nil {
@@ -497,7 +497,7 @@ func (db PostgreSQLDatabase) GetPlaylists(userID string) ([]types.Playlist, erro
 	return playlists, err
 }
 
-func (db PostgreSQLDatabase) GetAllPlaylists() ([]types.Playlist, error) {
+func (db *PostgreSQLDatabase) GetAllPlaylists() ([]types.Playlist, error) {
 	var playlists []types.Playlist
 	rows, err := db.sqlDB.Query("SELECT * FROM playlists;")
 	if err != nil {
@@ -515,7 +515,7 @@ func (db PostgreSQLDatabase) GetAllPlaylists() ([]types.Playlist, error) {
 	return playlists, err
 }
 
-func (db PostgreSQLDatabase) AddPlaylist(playlist types.Playlist) error {
+func (db *PostgreSQLDatabase) AddPlaylist(playlist types.Playlist) error {
 	_, err := db.sqlDB.Exec(`
 	INSERT INTO playlists (
 	  id, user_id, title, track_ids, listen_count, favorite_count, description, creation_date, addition_date, tags, additional_meta, permissions, metadata_source
@@ -526,7 +526,7 @@ func (db PostgreSQLDatabase) AddPlaylist(playlist types.Playlist) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UpdatePlaylist(playlist types.Playlist) error {
+func (db *PostgreSQLDatabase) UpdatePlaylist(playlist types.Playlist) error {
 	_, err := db.sqlDB.Exec(`
 	UPDATE playlists
 	SET user_id=$2, title=$3, track_ids=$4, listen_count=$5, favorite_count=$6, description=$7, creation_date=$8, addition_date=$9, tags=$10, additional_meta=$11, permissions=$12, metadata_source=$13
@@ -535,33 +535,33 @@ func (db PostgreSQLDatabase) UpdatePlaylist(playlist types.Playlist) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UsernameExists(username string) (bool, error) {
+func (db *PostgreSQLDatabase) UsernameExists(username string) (bool, error) {
 	var exists bool
 	err := db.sqlDB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE username=$1);", strings.ToLower(username)).Scan(&exists)
 	return exists, err
 }
 
-func (db PostgreSQLDatabase) EmailExists(email string) (bool, error) {
+func (db *PostgreSQLDatabase) EmailExists(email string) (bool, error) {
 	var exists bool
 	err := db.sqlDB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE email=$1);", strings.ToLower(email)).Scan(&exists)
 	return exists, err
 }
 
-func (db PostgreSQLDatabase) GetUser(id string) (types.User, error) {
+func (db *PostgreSQLDatabase) GetUser(id string) (types.User, error) {
 	user := types.User{}
 	row := db.sqlDB.QueryRow("SELECT * FROM users WHERE id=$1;", id)
 	err := row.Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash, &user.DisplayName, &user.Description, &user.ListenedTo, &user.Favorites, &user.PublicViewCount, &user.CreationDate, &user.Permissions, &user.LinkedArtistID, &user.LinkedSources)
 	return user, err
 }
 
-func (db PostgreSQLDatabase) GetUserByUsername(username string) (types.User, error) {
+func (db *PostgreSQLDatabase) GetUserByUsername(username string) (types.User, error) {
 	user := types.User{}
 	row := db.sqlDB.QueryRow("SELECT * FROM users WHERE username=$1 OR email=$1;", strings.ToLower(username))
 	err := row.Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash, &user.DisplayName, &user.Description, &user.ListenedTo, &user.Favorites, &user.PublicViewCount, &user.CreationDate, &user.Permissions, &user.LinkedArtistID, &user.LinkedSources)
 	return user, err
 }
 
-func (db PostgreSQLDatabase) GetUsers() ([]types.User, error) {
+func (db *PostgreSQLDatabase) GetUsers() ([]types.User, error) {
 	var users []types.User
 	rows, err := db.sqlDB.Query("SELECT * FROM users;")
 	if err != nil {
@@ -579,7 +579,7 @@ func (db PostgreSQLDatabase) GetUsers() ([]types.User, error) {
 	return users, err
 }
 
-func (db PostgreSQLDatabase) CreateUser(user types.User) error {
+func (db *PostgreSQLDatabase) CreateUser(user types.User) error {
 	_, err := db.sqlDB.Exec(`
 	INSERT INTO users (
 	  id, username, email, password_hash, display_name, description, listened_to, favorites, public_view_count, creation_date, permissions, linked_artist_id, linked_sources
@@ -590,7 +590,7 @@ func (db PostgreSQLDatabase) CreateUser(user types.User) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) UpdateUser(user types.User) error {
+func (db *PostgreSQLDatabase) UpdateUser(user types.User) error {
 	_, err := db.sqlDB.Exec(`
 	UPDATE users
 	SET username=$2, email=$3, password_hash=$4, display_name=$5, description=$6, listened_to=$7, favorites=$8, public_view_count=$9, creation_date=$10, permissions=$11, linked_artist_id=$12, linked_sources=$13
@@ -599,17 +599,17 @@ func (db PostgreSQLDatabase) UpdateUser(user types.User) error {
 	return err
 }
 
-func (db PostgreSQLDatabase) BlacklistToken(token string, expiration time.Time) error {
+func (db *PostgreSQLDatabase) BlacklistToken(token string, expiration time.Time) error {
 	_, err := db.sqlDB.Exec("INSERT INTO blacklisted_tokens (token, expiration) VALUES ($1, $2);", token, expiration)
 	return err
 }
 
-func (db PostgreSQLDatabase) CleanExpiredTokens() error {
+func (db *PostgreSQLDatabase) CleanExpiredTokens() error {
 	_, err := db.sqlDB.Exec("DELETE FROM blacklisted_tokens WHERE expiration < NOW();")
 	return err
 }
 
-func (db PostgreSQLDatabase) IsTokenBlacklisted(token string) (bool, error) {
+func (db *PostgreSQLDatabase) IsTokenBlacklisted(token string) (bool, error) {
 	var exists bool
 	err := db.sqlDB.QueryRow("SELECT EXISTS(SELECT 1 FROM blacklisted_tokens WHERE token=$1);", token).Scan(&exists)
 	return exists, err
