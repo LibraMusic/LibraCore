d02b4aa6ceeb2f12ce443e12037f29a15af7f475
Robert Soncrant <devreaper0@gmail.com>
2024-12-31 01:23:58 +0000

    fix: remove unused method receiver variables

--- api/json.go
+++ api/json.go
@@ -13,7 +13,7 @@ type GoJSONSerializer struct{}
 
 // Serialize converts an interface into a json and writes it to the response.
 // You can optionally use the indent parameter to produce pretty JSONs.
-func (d GoJSONSerializer) Serialize(c echo.Context, i interface{}, indent string) error {
+func (GoJSONSerializer) Serialize(c echo.Context, i interface{}, indent string) error {
 	enc := json.NewEncoder(c.Response())
 	if indent != "" {
 		enc.SetIndent("", indent)
@@ -22,7 +22,7 @@ func (d GoJSONSerializer) Serialize(c echo.Context, i interface{}, indent string
 }
 
 // Deserialize reads a JSON from a request body and converts it into an interface.
-func (d GoJSONSerializer) Deserialize(c echo.Context, i interface{}) error {
+func (GoJSONSerializer) Deserialize(c echo.Context, i interface{}) error {
 	err := json.NewDecoder(c.Request().Body).Decode(i)
 	if ute, ok := err.(*json.UnmarshalTypeError); ok {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Unmarshal type error: expected=%v, got=%v, field=%v, offset=%v", ute.Type, ute.Value, ute.Field, ute.Offset)).SetInternal(err)
