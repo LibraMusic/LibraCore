7553267296ec1e29b6dbfb780b2645f86dc13914
Robert Soncrant <devreaper0@gmail.com>
2025-03-14 20:08:03 -0500

    fix(metrics)!: replace "_count" gauges with "_total" counters for correctness

--- api/metrics/metrics.go
+++ api/metrics/metrics.go
@@ -9,86 +9,86 @@ import (
 )
 
 var (
-	TrackCount = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "libra_track_count",
+	TracksTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "libra_tracks_total",
 		Help: "Total number of tracks stored in the Libra database.",
 	})
-	AlbumCount = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "libra_album_count",
+	AlbumsTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "libra_albums_total",
 		Help: "Total number of albums stored in the Libra database.",
 	})
-	VideoCount = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "libra_video_count",
+	VideosTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "libra_videos_total",
 		Help: "Total number of videos stored in the Libra database.",
 	})
-	ArtistCount = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "libra_artist_count",
+	ArtistsTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "libra_artists_total",
 		Help: "Total number of artists stored in the Libra database.",
 	})
-	PlaylistCount = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "libra_playlist_count",
+	PlaylistsTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "libra_playlists_total",
 		Help: "Total number of playlists stored in the Libra database.",
 	})
-	UserCount = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "libra_user_count",
+	UsersTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "libra_users_total",
 		Help: "Total number of users registered in the Libra database.",
 	})
 )
 
 func RegisterMetrics() error {
-	if err := prometheus.Register(TrackCount); err != nil {
+	if err := prometheus.Register(TracksTotal); err != nil {
 		return err
 	}
 	tracks, err := db.DB.GetAllTracks(context.Background())
 	if err != nil {
 		return err
 	}
-	TrackCount.Set(float64(len(tracks)))
+	TracksTotal.Add(float64(len(tracks)))
 
-	if err := prometheus.Register(AlbumCount); err != nil {
+	if err := prometheus.Register(AlbumsTotal); err != nil {
 		return err
 	}
 	albums, err := db.DB.GetAllAlbums(context.Background())
 	if err != nil {
 		return err
 	}
-	AlbumCount.Set(float64(len(albums)))
+	AlbumsTotal.Add(float64(len(albums)))
 
-	if err := prometheus.Register(VideoCount); err != nil {
+	if err := prometheus.Register(VideosTotal); err != nil {
 		return err
 	}
 	videos, err := db.DB.GetAllVideos(context.Background())
 	if err != nil {
 		return err
 	}
-	VideoCount.Set(float64(len(videos)))
+	VideosTotal.Add(float64(len(videos)))
 
-	if err := prometheus.Register(ArtistCount); err != nil {
+	if err := prometheus.Register(ArtistsTotal); err != nil {
 		return err
 	}
 	artists, err := db.DB.GetAllArtists(context.Background())
 	if err != nil {
 		return err
 	}
-	ArtistCount.Set(float64(len(artists)))
+	ArtistsTotal.Add(float64(len(artists)))
 
-	if err := prometheus.Register(PlaylistCount); err != nil {
+	if err := prometheus.Register(PlaylistsTotal); err != nil {
 		return err
 	}
 	playlists, err := db.DB.GetAllPlaylists(context.Background())
 	if err != nil {
 		return err
 	}
-	PlaylistCount.Set(float64(len(playlists)))
+	PlaylistsTotal.Add(float64(len(playlists)))
 
-	if err := prometheus.Register(UserCount); err != nil {
+	if err := prometheus.Register(UsersTotal); err != nil {
 		return err
 	}
 	users, err := db.DB.GetUsers(context.Background())
 	if err != nil {
 		return err
 	}
-	UserCount.Set(float64(len(users)))
+	UsersTotal.Add(float64(len(users)))
 
 	return nil
 }
