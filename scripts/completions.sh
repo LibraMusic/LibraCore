8bf6fbd1ecde3e31694a5d32bb466a3b49656221
Robert Soncrant <devreaper0@gmail.com>
2025-11-13 13:55:03 -0600

    fix: use correct main package path in scripts

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run . completion "$sh" >"completions/libra.$sh"
+  go run ./cmd/libra completion "$sh" >"completions/libra.$sh"
 done
--- scripts/manpages.sh
+++ scripts/manpages.sh
@@ -2,4 +2,4 @@
 set -e
 rm -rf manpages
 mkdir manpages
-go run . man | gzip -c -9 >manpages/libra.1.gz
+go run ./cmd/libra man | gzip -c -9 >manpages/libra.1.gz
